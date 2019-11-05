3534ecbecae462a53da3b135335459d707795a08
ChrisLeOtaku <christopherfarrant1998@gmail.com>
2019-11-04 17:04:12 -0700

    Playlist Object Created

---
+++ src/main/java/farrant/christopher/api/controller/PlaylistRestController.java
@@ -0,0 +1,10 @@
+package farrant.christopher.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/playlist")
+public class PlaylistRestController {
+
+}
---
+++ src/main/java/farrant/christopher/api/model/Playlist.java
@@ -0,0 +1,62 @@
+package farrant.christopher.api.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="playlist")
+public class Playlist {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="playlist_id", nullable=false)
+	private int playlist_id;
+	
+	@Column(name="user_id", nullable=false)
+	private int user_id;
+	
+	@ManyToMany
+	@Column(name="songs_list", nullable=false)
+	private List<Song> songs;
+	
+	public Playlist() {
+		songs = new ArrayList<Song>();
+	}
+	
+	public Playlist(int user_id, List<Song> songs) {
+		this.user_id = user_id;
+		this.songs = songs;
+	}
+
+	public int getPlaylist_id() {
+		return playlist_id;
+	}
+
+	public void setPlaylist_id(int playlist_id) {
+		this.playlist_id = playlist_id;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public List<Song> getSongs() {
+		return songs;
+	}
+
+	public void setSongs(List<Song> songs) {
+		this.songs = songs;
+	}
+}
